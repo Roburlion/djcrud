da3fd466f0cf1aadbaa0ec1de6cf0e8539792086
Benito Calabria <benitorcalabria@gmail.com>
2025-03-10 19:51:35 -0500

    Add base template and HTMX page

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),  # Maps the root URL to the homepage
+    path('htmx_page', views.htmx_page, name='htmx_page'),
 ]
--- home/views.py
+++ home/views.py
@@ -11,6 +11,11 @@ def home_page(request):
     button_logs = ButtonLog.objects.all().order_by('-pressed_at')
     return render(request, 'home/home_page.html', {'button_logs': button_logs})
 
+def htmx_page(request):
+    # Get all ButtonLog entries, ordered by pressed_at descending (newest first)
+    button_logs = ButtonLog.objects.all().order_by('-pressed_at')
+    return render(request, 'home/htmx_page.html', {'button_logs': button_logs})
+
 def log_button(request):
     ButtonLog.objects.create()
     return HttpResponse("Button pressed and logged.")
