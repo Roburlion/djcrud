93eb45cd9d9b3ee7275221debfd5b6ae568065d9
Benito Calabria <benitorcalabria@gmail.com>
2025-03-13 07:09:25 -0500

    Register CustomerAging model in the admin panel

--- home/admin.py
+++ home/admin.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 # Register your models here.
 # from .models import Banner
 # admin.site.register(Banner)
-from .models import ButtonLog
+from .models import ButtonLog, CustomerAging
 admin.site.register(ButtonLog)
+admin.site.register(CustomerAging)
