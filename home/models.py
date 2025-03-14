a12a5d1c4aed06c8e0ccbe969c1e17df2825c51a
Benito Calabria <benitorcalabria@gmail.com>
2025-03-14 07:46:25 -0500

    Rename CustomerAging model to WOBs and add Workflow, Rolls, and RollBatches models

--- home/models.py
+++ home/models.py
@@ -9,7 +9,7 @@ class ButtonLog(models.Model):
             local_time = timezone.localtime(self.pressed_at)
             return f"{local_time:%Y-%m-%d %H:%M:%S}"
 
-class CustomerAging(models.Model):
+class WOBs(models.Model):
     wob = models.TextField(unique=True)
     batch = models.IntegerField(null=True)
     status = models.TextField()
@@ -20,3 +20,30 @@ class CustomerAging(models.Model):
 
     def __str__(self):
         return f"wob: {self.wob}, batch: {self.batch}, status: {self.status}, age: {self.age}, order_qty: {self.order_qty}, page_qty: {self.page_qty}"
+
+class Workflow(models.Model):
+    workflow = models.CharField(primary_key=True, max_length=16)
+    name = models.TextField()
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"workflow: {self.workflow}, name: {self.name}, deleted: {self.deleted}"
+
+class Rolls(models.Model):
+    roll = models.IntegerField(primary_key=True)
+    workflow = models.ForeignKey(Workflow, on_delete=models.PROTECT, related_name='rolls')
+    created_on = models.DateTimeField()
+
+    def __str__(self):
+        return f"roll: {self.roll}, workflow: {self.workflow}, created_on: {self.created_on}"
+
+class RollBatches(models.Model):
+    roll = models.ForeignKey(Rolls, on_delete=models.PROTECT, related_name='roll_batches')
+    batch = models.IntegerField()
+    created_on = models.DateTimeField()
+
+    class Meta:
+        unique_together = ('roll', 'batch')
+
+    def __str__(self):
+        return f"roll: {self.roll}, batch: {self.batch}, created_on: {self.created_on}"
