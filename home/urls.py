5d09d296704a48cdaea380ba8a3610004aab0620
Benito Calabria <benitorcalabria@gmail.com>
2025-03-15 06:52:28 -0500

    Add rolls_page URL path to urlpatterns

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('htmx_page', views.htmx_page, name='htmx_page'),
     path('upload_button_page', views.upload_button_page, name='upload_button_page'),
     path('upload_aging_page', views.upload_aging_page, name='upload_aging_page'),
+    path('rolls_page', views.rolls_page, name='rolls_page'),
     # APIs
     path('button_upload', views.button_upload, name='button_upload'),
     path('customer_aging_upload', views.customer_aging_upload, name='customer_aging_upload'),
