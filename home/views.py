83e2e756b7f7e33774deb3b08feed8d4cf016db6
Benito Calabria <benitorcalabria@gmail.com>
2025-03-15 11:05:06 -0500

    Enhance rolls_page to include wobs and rolls data retrieval

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from .utils_button import button_upload, get_button_logs, log_button, log_htmx_button
-from .utils_rocket import upload_wobs, get_wobs
+from .utils_rocket import upload_wobs, get_wobs, get_rolls, get_workflows, get_roll_batches
 
 def home_page(request):
     # Get updated logs
@@ -21,4 +21,8 @@ def upload_aging_page(request):
     return render(request, 'home/upload_aging_page.html')
 
 def rolls_page(request):
-    return render(request, 'home/rolls_page.html')
+    wobs = get_wobs()
+    rolls = get_rolls()
+    print(f"-----------wobs: {wobs}")
+    print(f"-----------rolls: {rolls}")
+    return render(request, 'home/rolls_page.html', {'wobs': wobs, 'rolls': rolls})
