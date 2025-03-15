bcf00de3b4b0aeb942ee05d9589fd27a44a7b0ed
Benito Calabria <benitorcalabria@gmail.com>
2025-03-15 07:17:42 -0500

    Rename get_customer_aging to get_wobs in views.py

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from .button_utils import button_upload, get_button_logs, log_button, log_htmx_button
-from .aging_utils import customer_aging_upload, get_customer_aging
+from .aging_utils import customer_aging_upload, get_wobs
 
 def home_page(request):
     # Get updated logs
